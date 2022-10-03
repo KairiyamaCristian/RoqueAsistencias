04c07607e859751032086fcd6507e8a2372d4d24
KairiyamaCristian <cristiankairiyama@gmail.com>
2022-10-03 18:10:45 -0300

    cambios

--- Backend/asist_profe_view/models.py
+++ Backend/asist_profe_view/models.py
@@ -2,14 +2,14 @@
 from django.db import models
 
 class Materia(models.Model):
-    nombre = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=30)
 
     def __str__(self) -> str:
         return self.nombre
     
 class Profesor(models.Model):
-    nombre = models.CharField(max_length=20)
-    correo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=30)
+    correo = models.CharField(max_length=50)
     direccion = models.CharField(max_length=50)
     materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
 
@@ -17,8 +17,8 @@ class Profesor(models.Model):
         return self.nombre
 
 class Alumno(models.Model):
-    nombre = models.CharField(max_length=20)
-    correo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=30)
+    correo = models.CharField(max_length=50)
     direccion = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -28,13 +28,13 @@ class Asistencia(models.Model):
     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
 
 class Fecha(models.Model):
-    tipo = models.CharField(max_length=20)
+    tipo = models.CharField(max_length=30)
     asistencia = models.ForeignKey(Asistencia, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.tipo
 class Estado(models.Model):
-    tipo = models.CharField(max_length=20)
+    tipo = models.CharField(max_length=30)
     asistencia = models.ForeignKey(Asistencia, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
