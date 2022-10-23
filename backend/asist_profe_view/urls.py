c6c7a6d6bc8ef5d686e4994cde9a43f482bcd0f8
KairiyamaCristian <cristiankairiyama@gmail.com>
2022-10-23 20:08:59 -0300

    algo

--- backend/asist_profe_view/urls.py
+++ backend/asist_profe_view/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-
+    path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("login/materia/<int:materia_id>/", views.registro_materias, name="Materias"),
+    path("login/materia/<int:alumno_id>/", views.registro_asistencia_por_materia, name="Alumnos"),
 ]
