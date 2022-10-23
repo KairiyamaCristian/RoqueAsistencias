5b2c24c1496528e9c7f16f9c8d5bd9a8f7684f1c
KairiyamaCristian <cristiankairiyama@gmail.com>
2022-10-23 19:24:02 -0300

    agregando vistas

--- Backend/asist_profe_view/views.py
+++ Backend/asist_profe_view/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello word")
+
+def login(request):
+    return HttpResponse("esto es el login")
+
+def registro_materias(request):
+    return HttpResponse("esto es materias")
+
+def registro_asistencia_por_materia(request):
+    return HttpResponse("esto es registro de asistencia por materia")
