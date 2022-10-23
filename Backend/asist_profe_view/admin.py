5dcd77e8102b2aba56c09cab5a93f3ecc11dfada
KairiyamaCristian <cristiankairiyama@gmail.com>
2022-10-23 19:14:32 -0300

    no se

--- Backend/asist_profe_view/admin.py
+++ Backend/asist_profe_view/admin.py
@@ -5,5 +5,3 @@ from .models import Materia, Profesor, Alumno, Asistencia, Fecha, Estado
 
 admin.site.register([Materia, Profesor, Alumno, Asistencia, Fecha, Estado])
 
-
-admin.site.register([Materia, Profesor, Alumno, Asistencia, Fecha, Estado])
