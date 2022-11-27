144d3291e7f08d033b6333df846761ab96e9338e
KairiyamaCristian <cristiankairiyama@gmail.com>
2022-11-27 11:58:20 -0300

    agregando nuevas clases

--- backend/asist_profe_view/models.py
+++ backend/asist_profe_view/models.py
@@ -2,41 +2,44 @@
 from email.policy import default
 from django.db import models
 
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=20)
+    correo = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.nombre    
+
 class Materia(models.Model):
     nombre = models.CharField(max_length=20)
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.nombre
     
-# class Profesor(models.Model):
-#     nombre = models.CharField(max_length=20)
-#     correo = models.CharField(max_length=20)
-#     direccion = models.CharField(max_length=50)
-#     materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
-
-#     def __str__(self) -> str:
-#         return self.nombre
-
 class Alumno(models.Model):
-    materia = models.ForeignKey(Materia, on_delete = models.CASCADE)
     nombre = models.CharField(max_length=20)
+    materia = models.ForeignKey(Materia, on_delete = models.CASCADE)
     asistencia = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return self.nombre
 
-# class Asistencia(models.Model):
-#     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+class Fecha(models.Model):
+    tipo = models.CharField(max_length=20)
 
-# class Fecha(models.Model):
-#     tipo = models.CharField(max_length=20)
-#     asistencia = models.ForeignKey(Asistencia, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.tipo
 
-#     def __str__(self) -> str:
-#         return self.tipo
-# class Estado(models.Model):
-#     tipo = models.CharField(max_length=20)
-#     asistencia = models.ForeignKey(Asistencia, on_delete=models.CASCADE)
+class Estado(models.Model):
+    tipo = models.CharField(max_length=20)
 
-#     def __str__(self) -> str:
-        # return self.tipo
+    def __str__(self) -> str:
+        return self.tipo
+
+class Asistencia(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    fecha = models.ForeignKey(Fecha, on_delete=models.CASCADE)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+
+
